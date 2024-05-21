bedc5d845be68d3c8850c4671baf463d913d6b4a
thederpykrafter <thederpykrafter@gmail.com>
2024-05-21 10:06:36 -0700

    add prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,51 @@
-# Start oh-my-posh with shell for nerd font support
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kali.omp.json" | Invoke-Expression
-# amro, chips, di4am0nd, easy-term, emodipt-extend, kali, 
+# Prompt
+# Colors
+$BLUE_FG = "`e[34m"
+$GREEN_FG = "`e[32m"
 
-# PowerToys CommandNotFound module
-Import-Module "C:\Users\thede\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+function prompt_username {
+        Write-Output "┌─$BLUE_FG$($env:USERNAME)`e[0m`e"
+}
+
+function prompt_hostname {
+        Write-Output "$GREEN_FG$($env:COMPUTERNAME)`e[0m─┐"
+}
+
+function prompt_divider {
+        $prompt_divider_width = $env:USERNAME.Length + $env:COMPUTERNAME.Length + 3
+        Write-Output "$('─' * ($Host.UI.RawUI.WindowSize.Width - $prompt_divider_width))`e"
+}
+
+function get_prompt_directory {
+        Write-Output "$PWD"
+}
+
+function prompt_directory_divider {
+        Write-Output "$(' ' * ($Host.UI.RawUI.WindowSize.Width - $(get_prompt_directory).Length - 3)) ┘"
+}
+
+function prompt_directory {
+        Write-Output "├$($PWD)$(prompt_directory_divider)"
+}
+
+
+function prompt {
+        "$(prompt_username) $(prompt_divider) $(prompt_hostname)
+$(prompt_directory)
+└$('>' * ($nestedPromptLevel + 1)) "
+}
+
+# Start oh-my-posh with shell for nerd font support
+#oh-my-posh init pwsh --config "$config\Oh-My-Posh\darkblood.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\darkblood2.omp.json" | Invoke-Expression
+# amro, chips, di4am0nd, easy-term, emodipt-extend, kali, sim-web
+
+# Remove VS Code Insiders Shortcut from Desktop
+# Only nescesarry because installed from winget
+if ( Test-Path "C:\Users\Public\Desktop\Visual Studio Code - Insiders.lnk" ) 
+{
+  Remove-Item "C:\Users\Public\Desktop\Visual Studio Code - Insiders.lnk"
+}
 
 # Shortcut directory environment variables
 $wsl = "\\wsl.localhost\Ubuntu-24.04\home\thederpykrafter\"
@@ -66,3 +108,8 @@ function remove-empty-dirs {
 
 #Clear-Host # Remove if error gets hidden
 tdk_uptime # Check uptime on launch/reload
+
+
+#34de4b3d-13a8-4540-b76d-b9e8d3851756 PowerToys CommandNotFound module
+Import-Module "C:\Users\thede\AppData\Local\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+#34de4b3d-13a8-4540-b76d-b9e8d3851756
